76beb6379e168802f540805520d786a128e19cc3
Joaquin Alegre <joaquin.cpp@gmail.com>
2021-06-07 22:59:53 -0400

    Add Tabs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Header } from './components/Header';
+import { Tabs } from './components/Tabs';
+import classes from './styles.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className={classes.app}>
+      <section>
+        <Header />
+      </section>
+      <section className={classes.tabsSection}>
+        <Tabs />
+      </section>
     </div>
   );
 }
---
+++ src/components/Tabs/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import classes from './styles.module.css'
+
+export const Tabs = () => {
+    const [tab, setTab] = useState(0);
+    return (
+        <div className={classes.tabsContainer}>
+            <button type="button" className={tab === 0 ? classes.buttonActive : null } onClick={() => setTab(0)}>All</button>
+            <button type="button" className={tab === 1 ? classes.buttonActive : null } onClick={() => setTab(1)}>My faves</button>
+        </div>
+    );
+};

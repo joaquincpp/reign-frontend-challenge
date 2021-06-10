27bdcf17c24b35d8556794bcd0a7c7164fe455d5
Joaquin Alegre <joaquin.cpp@gmail.com>
2021-06-10 05:51:24 -0400

    Add event listener for outside clicking on dropdown selector

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, {
+  useRef, useEffect, useState, useContext,
+} from 'react';
 import { AppContext } from '../Provider';
 import classes from './styles.module.css';
 import AngularLogo from '../../assets/angular-logo.png';
@@ -8,6 +10,27 @@ import VueLogo from '../../assets/vue-logo.png';
 const Dropdown = () => {
   const [state, setState] = useContext(AppContext);
   const [dropdown, setDropdown] = useState(false);
+  const wrapperRef = useRef(null);
+
+  // Function that closes the dropdown when the user clicks elsewhere on the website.
+  const useOutsideDetection = (ref) => {
+    useEffect(() => {
+      // Closes dropdown if clicked outside of it.
+      const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setDropdown(false);
+        }
+      };
+      // Bind the event listener
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+      // Unbind the event listener on clean up
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [ref]);
+  };
+
+  useOutsideDetection(wrapperRef);
 
   // Handles the option selection to store it on the global context and localStorage,
   // and closes the dropdown.
@@ -50,7 +73,7 @@ const Dropdown = () => {
           </button>
           {/* Shows the dropdown options in case the dropdown button was clicked. */}
           {dropdown === true && (
-          <div className={classes.optionsSelector}>
+          <div className={classes.optionsSelector} ref={wrapperRef}>
             {options.map((element) => (
               <div
                 key={element.name}
